66aaf5bcfbd123661fe38b604447e8b40daab065
Okash Khawaja <okash.khawaja@gmail.com>
2016-04-09 21:14:56 +0100

    minor updates

--- ddns-updater.lua
+++ ddns-updater.lua
@@ -4,6 +4,8 @@ local http = require 'socket.http'
 http.USERAGENT="ddns-updater/0.1 okash.khawaja@gmail.com"
 
 -- values plugged in the URL below must be URL-encoded
-local r, c, h, s = https.request("http://username:password@dynupdate.no-ip.com/nic/update?hostname=dnsname.net&myip=127.0.0.1")
-print(r, c, h, s)
+local response, code, headers, status = https.request(
+	"https://username:password@dynupdate.no-ip.com/nic/update?
+				hostname=dnsname.net&myip=127.0.0.1")
+print(response, status)
 
