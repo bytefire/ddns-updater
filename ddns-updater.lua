bdcb754827157a59c1b6aae758daceaadb7c33d6
Okash Khawaja <okash.khawaja@gmail.com>
2016-04-10 16:51:47 +0100

    add handling of last-update file

--- ddns-updater.lua
+++ ddns-updater.lua
@@ -1,7 +1,59 @@
 local https = require 'ssl.https'
 local http = require 'socket.http'
 
+local RESPONSE_911 = "911"
+local RETRY_SECONDS_911 = 30 * 60 -- wait at least 30 mins before retrying after receiving 911 from noip.com
+local LAST_UPDATE_FILE_PATH = "last-update"
+
 http.USERAGENT="ddns-updater/0.1 okash.khawaja@gmail.com"
+
+local function read_whole_file(path)
+        local f = io.open(path, "r")
+        if not f then
+                return nil
+        end
+
+        local contents = f:read("*all")
+        f:close()
+
+        return contents
+end
+
+local function read_last_update()
+        local last_update = {}
+        local str = read_whole_file(LAST_UPDATE_FILE_PATH)
+
+        if not str then
+                last_update.time = 0
+                last_update.ip = ""
+                last_update.response = ""
+
+                return last_update
+        end
+
+        -- time
+        local start_index = 1
+        local space_index = string.find(str, " ")
+        last_update.time = tonumber(string.sub(str, start_index, space_index - 1))
+
+	-- ip
+        start_index = space_index + 1
+        space_index = string.find(str, " ", start_index)
+        last_update.ip = string.sub(str, start_index, space_index - 1)
+
+        -- response
+        last_update.response = string.sub(str, space_index + 1)
+
+        return last_update
+end
+
+local function write_last_update(last_update)
+        local str = last_update.time .. " " .. " " .. last_update.ip ..
+                " " .. " " .. last_update.response
+        local f = io.open(LAST_UPDATE_FILE_PATH, "w")
+        f:write(str)
+        f:close()
+end
 
 -- values plugged in the URL below must be URL-encoded
 local response, code, headers, status = https.request(
